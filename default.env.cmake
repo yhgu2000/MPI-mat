948780009f2e18ff24188ff119c3c1ddb264c92a
nsccgz_xwzhang_1 <yhgu2000@outlook.com>
2024-03-23 12:33:23 +0800

    添加MPI路径配置

--- default.env.cmake
+++ default.env.cmake
@@ -1,7 +1,12 @@
 # 覆盖率测试
-set(CODE_COVERAGE ON)
+#set(CODE_COVERAGE ON)
 
 # BOOST 路径
-set(Boost_NO_SYSTEM_PATHS ON)
-set(BOOST_ROOT "C:/Libraries/boost/1_81_0")
-# set(Boost_DEBUG ON)
+#set(Boost_NO_SYSTEM_PATHS ON)
+#set(Boost_NO_WARN_NEW_VERSIONS ON)
+#set(BOOST_ROOT "C:/Libraries/boost/1_81_0")
+#set(Boost_DEBUG ON)
+
+# MPI 路径
+#set(MPIEXEC_EXECUTABLE "" CACHE PATH "")
+
